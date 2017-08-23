7e9428580874c9c9a1969fcb8648511720aecc61
yanshaozhuo <yanshaozhuo@126.com>
2017-08-23 22:55:26 +0800

    change page's title and content.

--- spider_douban.py
+++ spider_douban.py
@@ -1,4 +1,4 @@
-#coding ='utf-8'
+
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -12,7 +12,6 @@ def page():
     soup = BeautifulSoup(response.text,'lxml')
     soup_page = soup.find_all('table',class_="tagCol")
     url_all = []
-    #print(soup_page.a)
     for each in soup_page:
         each_page = each.find_all('a')
         for i in each_page:
@@ -21,6 +20,7 @@ def page():
 
     return url_all
 
+
 def one_page(url_all):
     for each in url_all:
         response1 = requests.get(each,headers = headers)
@@ -28,9 +28,24 @@ def one_page(url_all):
         soup1_page = soup1.find('ul',class_="subject-list").find_all('h2',class_='')
         with open('url_page.txt','a+') as f:
             for i in soup1_page:
-                f.write(i.a['href'])
-                f.write('\n')
+                url =i.a['href']
+                response1 = requests.get(url, headers=headers)
+                soup1 = BeautifulSoup(response1.text, 'lxml')
+                soup1_title = soup1.find('span',property="v:itemreviewed").text
+                soup1_content = soup1.find('div',class_="intro").text
+                print(soup1_title)
+                print(soup1_content)
+                print(url)
 
+
+
+
+'''
+def loading_url():
+    with open('url_page.txt','a') as f:
+        response1 = requests.get(f.readline(),headers = headers)
+        soup1 = BeautifulSoup(response1.text,'lxml')
+'''
 
 
 
